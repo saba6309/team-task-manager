454c9628e55d506b1cd8310f8978c06f666eb6aa
saba6309 <shafiasaba6309@gmail.com>
2026-05-10 19:54:37 +0530

    switched to postgres

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
@@ -131,14 +132,3 @@ MIDDLEWARE.insert(
 #STATICFILES_STORAGE = (
 #    'whitenoise.storage.CompressedStaticFilesStorage'
 #)
-
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
-    }
-}
